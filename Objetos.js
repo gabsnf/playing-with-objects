f510a734d26aa27370865e349c6a134e8503a6ad
Gabriel Francisco Xavier <91631321+gabsnf@users.noreply.github.com>
2022-05-16 13:50:12 -0300

    Add files via upload

---
+++ Objetos.js
@@ -0,0 +1,59 @@
+const user={
+    Nome:"Gabriel Xavier",
+    Endereco:{
+        Rua:"Rua Pedrinhas",
+        Numero:201,
+        Complemento:"Adega alguma coisa",
+    },
+    Email:"gabrielxavier0070@gmail.com",
+    Idade:18
+}
+
+const user1={
+    Nome:"Gabriel Xavier",
+    Endereco:{
+        Rua:"fabricio fontana",
+        Numero:21,
+        Complemento:"cimento",
+    },
+    Email:"gabrielxavier0070@gmail.com",
+    Idade:28
+
+
+
+}
+
+for(let i=0;i<11;i++){
+    console.log(i)
+if(i===5){
+    console.log("chegou no 5")
+
+}
+else{console.log("o i nao eh igual a 5")}
+} 
+
+let umbigo=[1,2,3,4,5,6,7,8,9,10]
+console.log(umbigo.length)
+console.log(umbigo[0])
+
+for(let j=0;j<10;j++){
+    console.log(umbigo[j])
+}
+
+const user2={
+    name:"gabs",
+    idade:18,
+    sabor:"morango",
+    vigesimo:20,
+    sexto:{
+        primeiro:"segundo",
+        terceiro:"quarto",
+        quinto:"sexto",
+        setimo:"oitavo",
+        decimo:"decimo primeiro",
+    },
+    tambor:"bateria",
+    pyongyang:"seoul"
+}
+console.log(user2.sexto.decimo)
+console.log(user2.idade)
